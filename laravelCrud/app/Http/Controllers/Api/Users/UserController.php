fb940e4462c2d1e328aeaaca31554581f4d13a7b
mohammad azzam <mohammad.azzam.developer@gmail.com>
2023-07-02 23:45:08 +0300

    initial commit

---
+++ laravelCrud/app/Http/Controllers/Api/Users/UserController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Api\Users;
+
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+
+
+
+}
---
+++ laravelCrud/tests/Unit/Http/Controllers/Api/Orders/OrderControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Http\Controllers\Api\Orders;
+
+use App\Http\Controllers\Api\Orders\OrderController;
+use PHPUnit\Framework\TestCase;
+
+class OrderControllerTest extends TestCase
+{
+
+    public function testIndex()
+    {
+
+    }
+}
